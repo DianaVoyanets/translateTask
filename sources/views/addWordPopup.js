195cd2ed12ede507cc68e5fa12ef844b883374b3
Diana Voyanets <diana.voyanets@gmail.com>
2018-09-13 10:06:35 +0300

    change addWordPopup

--- sources/views/addWordPopup.js
+++ sources/views/addWordPopup.js
@@ -3,66 +3,61 @@ import {baseOfWordsCollection} from "models/baseOfWordsCollection";
 import {wordsGroup} from "models/wordsGroup";
 
 export default class addWordsPopupView extends JetView{
-	config() {		
-		let popup = {
+	config() {	
+        
+		return  {
 			view:"window", 
-            height: 500,
-            modal: true,
+			width: 500,
+			height: 400,
+			scroll: false,
+			modal: true,
 			move:true,
 			localId: "formPopup",
-			head:"Add group of words",
+			head:"Edit group of words",
 			position:"center",
 			body:{
-                    view: "form",
-                    localId: "myform",
-                    select:"row",
-                    multiselect:true,
-                    width: 600,
-                    elements: [
-                        {view: "text",labelWidth: 120,label: "Name of group:",name: "name"},
-                        {view: "datatable",localId: "mydatatable",
-                        columns: [
-                            {id: "originWords",header: "Origin word"},
-                            {id: "translation",header: "Translation"},
-                            {id: "partOfSpeach",header: "Part of speach"},      
-                        ],
-                    },
+				view: "form",
+				localId: "myform",
+				elements: [
+					{view: "text",labelWidth: 120,label: "Name of group:",name: "name"},
+					{view: "datatable",id:"mydatatable",select:"row",multiselect: true,width:500,scrollY:false,
+						columns: [
+							{id: "originWords",header: "Origin word",width: 150},
+							{id: "translation",header: "Translation"},
+							{id: "partOfSpeach",header: "Part of speach",width: 150},    
+						],
+					},
 					{cols:[
 						{view: "spacer"},
-                        {view: "button",value: "Add",width: 120,click: () => {
-                            let group = this.$$("form").getValues();
-							group.values = this.$$("mydatatable").getSelectedItem();
-							if(group.hasOwnProperty("id")){
-								wordsGroup.updateItem(group.id, group);
-							}
-							else{
-								wordsGroup.add(group);
-							}
-                        }},
+						{view: "button",value: "Add",width: 120,click: () => {
+							let group = this.$$("myform").getValues();
+							group.wordsIds = this.$$("mydatatable").getSelectedItem();
+							wordsGroup.updateItem(group.id,group);
+							this.$$("formPopup").hide();
+						}},
                         
 						{view: "button",value: "Cancel",width: 120,click: () => {
-                            this.$$("formPopup").hide();
-                        }}
+							this.$$("formPopup").hide();
+						}}
 					]}  
 				]
 				
 			}
-        }
-        return popup;
-
-    }
-	
-	showWindowWord(item) {
-        this.$$("mydatatable").unselectAll();
-        if(Array.isArray(item)) {
-            for(var i = 0;i < item.length;i++) {
-                this.$$("mydatatable").select(item[i].id,true);
-            } 
-        } else {
-            this.$$("mydatatable").select(item.id)
-        }
-		this.getRoot().show();
+		};
 	}
+	
+	showWindow(words,item) {
+		this.$$("mydatatable").unselectAll();
+		if (!words) {
+			this.$$("myform").setValues(item);
+			this.getRoot().show();
+			return;
+		}
+        
+		this.$$("myform").setValues(item);
+		this.$$("mydatatable").filter((item) => words.find(i => i.id === item.id) == undefined);
+		this.getRoot().show();
+	}	
 	
 	init() {
 		this.$$("mydatatable").sync(baseOfWordsCollection);
