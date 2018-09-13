d116299f091aa09b8a149737958f6d5636bdf48e
Diana Voyanets <diana.voyanets@gmail.com>
2018-09-13 10:09:40 +0300

    change db file

--- db.js
+++ db.js
@@ -8,36 +8,36 @@ var sequelize = new Sequelize("sampledb","root","1",{
 
 var Users = sequelize.define("users",{
 	login: Sequelize.STRING,
-    password: Sequelize.STRING
-},
-);
+	password: Sequelize.STRING
+});
+
 
 var Words = sequelize.define("words",{
-    originWords: Sequelize.STRING,
-    translation: Sequelize.STRING, 
-    partOfSpeach: Sequelize.STRING,
-})
+	originWords: Sequelize.STRING,
+	translation: Sequelize.STRING, 
+	partOfSpeach: Sequelize.STRING,
+});
 
 var wordsGroup = sequelize.define("wordsGroup",{
-    name: Sequelize.STRING,
-    dateOfCreation: Sequelize.DATE,
-    wordsIds: [
-        {
-            type: Sequelize.INTEGER,
-            references: {
-                model: "words",
-                key: 'id',
-                deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            },
-        }
-    ] 
-})
+	name: Sequelize.STRING,
+	dateOfCreation: Sequelize.DATE,
+	wordsIds: [
+		{
+			type: Sequelize.INTEGER,
+			references: {
+				model: "words",
+				key: "id",
+				deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+			},
+		}
+	] 
+});
 
 sequelize.sync({ force: true }).then(() => {
-    Words.create({originWords:"Привет",translation: "Hello",partOfSpeach:"существительное"});
+	Words.create({originWords:"Привет",translation: "Hello",partOfSpeach:"существительное"});
 });
 
 
 module.exports = {
-    Users,Words,wordsGroup
+	Users,Words,wordsGroup
 };
