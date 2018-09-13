f4822727e59d86159b4f0c4756d525cde995a6eb
Diana Voyanets <diana.voyanets@gmail.com>
2018-09-13 10:08:22 +0300

    change baseOfWords file

--- sources/views/baseOfWords.js
+++ sources/views/baseOfWords.js
@@ -1,18 +1,18 @@
 import {JetView} from "webix-jet";
 import {baseOfWordsCollection} from "models/baseOfWordsCollection";
 export default class BaseOfWords extends JetView {
-    config() {
-        var baseOfWords = {
-            view: "datatable",
-            localId: "mydatatable",
-            columns: [
-                {id: "originWords",header: "Origin word"},
-                {id: "translation",header: "Translation"},
-                {id: "partOfSpeach",header: "Part of speach"},
-                {id: "pencil-icon", header:"",template: "{common.editIcon()}",width:50},
-                {id: "trash-icon", header: "",template: "{common.trashIcon()}",width:50},            
-            ],
-            onClick: {
+	config() {
+		var baseOfWords = {
+			view: "datatable",
+			localId: "mydatatable",
+			columns: [
+				{id: "originWords",header: "Origin word"},
+				{id: "translation",header: "Translation"},
+				{id: "partOfSpeach",header: "Part of speach",width:150},
+				{id: "pencil-icon", header:"",template: "{common.editIcon()}",width:50},
+				{id: "trash-icon", header: "",template: "{common.trashIcon()}",width:50},            
+			],
+			onClick: {
 				"fa-trash": function(e, id) {
 					webix.confirm({
 						text:"Do you still want to remove field?",
@@ -25,35 +25,35 @@ export default class BaseOfWords extends JetView {
 					});
 				},
 			},
-        }
+		};
 
-        var form  = {
-            view: "form",
-            localId: "myform",
-            width: 400,
+		var form  = {
+			view: "form",
+			localId: "myform",
+			width: 400,
 			margin:20,
 			elements:[
 				{view:"text", name: "originWords",labelWidth: 110,label:"Oringin word:" },
 				{view:"text",name:  "translation", labelWidth: 110,label:"Translation:" },
-                {view: "text",name: "partOfSpeach",labelWidth: 110,label: "Part of speech:"},
+				{view: "combo",name: "partOfSpeach",labelWidth: 110,label: "Part of speech:"},
                 
 				{cols:[
-                    { view:"button", value:"Add new word",
-                    click:  (id) => {
-                        var data = this.$$("myform").getValues();
-                        baseOfWordsCollection.add(data);
-                        this.$$("myform").clear();
-                    }},
+					{ view:"button", value:"Add new word",
+						click:  (id) => {
+							var data = this.$$("myform").getValues();
+							baseOfWordsCollection.add(data);
+							this.$$("myform").clear();
+						}},
 				]},
 				{view: "spacer"}
 			]
 		};
     
-    return {
-        cols: [baseOfWords,form]
-    };
-}
+		return {
+			cols: [baseOfWords,form]
+		};
+	}
 
-init() {
-    this.$$("mydatatable").sync(baseOfWordsCollection);
-}}
+	init() {
+		this.$$("mydatatable").sync(baseOfWordsCollection);
+	}}
